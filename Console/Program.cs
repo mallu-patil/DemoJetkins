fc6e57a7b2d2722667abb1b5661da40885714d14
Mallanagouda Patil <Mallanagouda.Patil@emc.com>
2014-07-11 22:53:09 +0530

    initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    public delegate void Handle(string msg);
+
+    class Hash
+    {
+        public Func<string> Add { get; set; }
+        public Handle MyHandle { get; set; }
+
+        public event Action<string> MyAction;
+
+        public void StartAction()
+        {
+            MyAction("Hello Hi Dear");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Action<string,int> action = new Action<string,int>((msg,var) => { System.Console.WriteLine(msg); });
+            action("Hello",1);
+
+            Func<Hash> fun = () => new Hash();
+
+            Func<int> fun1 = () => 1;
+
+            System.Console.WriteLine(fun1());
+
+            Hash h = new Hash();
+            h.MyAction +=   (msg)=>{System.Console.WriteLine(msg);};
+
+            h.StartAction(); 
+        }
+
+
+        public static void PrintHandle(string msg)
+        {
+            System.Console.WriteLine(msg);
+        }
+    }
+}
---
+++ MathLibTest/MathTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace MathLibTest
+{
+    [TestFixture]
+    public class MathTest
+    {
+
+        [Test]
+        public void TestAdd()
+        {
+            MathLib.Math obj = new MathLib.Math();
+
+            Assert.AreEqual(obj.Add(10, 10), 20);
+        }
+        [Test]
+        public void TestSub()
+        {
+            MathLib.Math obj = new MathLib.Math();
+            Assert.AreEqual(obj.Sub(20, 10), 10);
+        }
+    }
+}
